3e9a7e868af6a5bb3a4fb63d9fcfbdd1e5d82524
anton <asugolov@gmail.com>
2022-06-09 14:50:48 -0400

    changes

--- grid/main.py
+++ grid/main.py
@@ -4,15 +4,24 @@ import random
 
 # we train a model on the usual cartpole example
 
+
+
+
+
 if __name__ == "__main__":
     env = gym.make('CartPole-v1')
     state = env.reset()
 
-    n = 10
+    n = 100
+    # 0, move left
+    # 1, move right
+
     for i in range(n):
         action = env.action_space.sample()
-        env.step(action)
+        next_state, reward, done, info = env.step(action)
+        print(i, next_state, reward, done, info, action)
         env.render()
+
     env.close()
 
     print(env.action_space)
