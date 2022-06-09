a56a30b87bbbf818184bedfae16faba8e9bc15dc
anton <asugolov@gmail.com>
2022-06-09 16:14:49 -0400

    added model() function

--- grid/main.py
+++ grid/main.py
@@ -1,11 +1,31 @@
 import gym
-import numpy
+import numpy as np
 import random
+
+import tensorflow as tf
+import tensorflow.keras as tfk
+
 
 # we train a model on the usual cartpole example
 
+def model(input_shape, action_space):
+    model = tfk.Sequential()
+    model.add(tfk.Input(shape=input_shape))
+    model.add(tfk.Dense(512, activation=tf.nn.relu,
+                        initializers=tf.keras.initializers.he_uniform))
+    model.add(tfk.Dense(256, activation=tf.nn.relu,
+                        initializers=tf.keras.initializers.he_uniform))
+    model.add(tfk.Dense(64, activation=tf.nn.relu,
+                        initializers=tf.keras.initializers.he_uniform))
+    model.add(tfk.Dense(action_space, activation=tf.nn.relu,
+                        initializers=tf.keras.initializers.he_uniform))
+    # mean squared error loss
+    # default RMS prop parameters: https://pylessons.com/CartPole-reinforcement-learning
 
+    model.compile(loss="mse", optimizer=tfk.optimizers.RMSprop(), metrics=["accuracy"])
+    model.summary()
 
+    return model
 
 
 if __name__ == "__main__":
@@ -26,4 +46,3 @@ if __name__ == "__main__":
 
     print(env.action_space)
     print(env.observation_space)
-
